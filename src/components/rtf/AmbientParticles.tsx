f6173e5bc3a59e6fdbc2fc916b658b7e90bae83a
Dango <jason543211@hotmail.com>
2025-05-02 16:25:56 -0700

    AmbientParticles Background Environment

--- src/components/rtf/AmbientParticles.tsx
+++ src/components/rtf/AmbientParticles.tsx
@@ -7,7 +7,7 @@ import { Environment, PerspectiveCamera } from '@react-three/drei'
 const AmbientParticles = () => {
   return (
     <div className="w-full h-screen fixed inset-0 -z-10 pointer-events-none p-10">
-      <Canvas shadows className="rounded-xl overflow-hidden">
+      <Canvas shadows className="rounded-lg overflow-hidden">
         <Sparkles count={350} scale={5} size={2.5} speed={0.25} color={''} />
         <Environment preset={'apartment'} background={true} backgroundBlurriness={1} />
         {/* <directionalLight position={[0, 0, 5]} intensity={1} castShadow /> */}
