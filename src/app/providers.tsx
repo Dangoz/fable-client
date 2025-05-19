ffda55c5748c4a7f38f0a2de8bf557522e127318
Dango <jason543211@hotmail.com>
2025-05-18 23:42:50 -0700

    web3 provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,16 +5,16 @@ import PrivyProvider from './_providers/privy-provider'
 import Stats from '@/components/common/stats'
 import QueryClientProvider from './_providers/query-client-provider'
 import { ConnectionProvider } from '@/context/ConnectionContext'
-
+import { Web3Provider } from './_providers/web3-provider'
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <QueryClientProvider>
         <ConnectionProvider>
-          <PrivyProvider>
+          <Web3Provider>
             {children}
             {process.env.NODE_ENV !== 'production' && process.env.NEXT_PUBLIC_STATS !== 'false' && <Stats />}
-          </PrivyProvider>
+          </Web3Provider>
         </ConnectionProvider>
       </QueryClientProvider>
     </>
