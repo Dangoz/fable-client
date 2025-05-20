303470193a7770048f0bd0146b0d57df5545a0fc
Dango <jason543211@hotmail.com>
2025-05-19 20:38:45 -0700

    landing

--- src/components/rtf/ambient-particles.tsx
+++ src/components/rtf/ambient-particles.tsx
@@ -2,14 +2,14 @@
 
 import { Sparkles } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-import { Environment, PerspectiveCamera } from '@react-three/drei'
+import { PerspectiveCamera } from '@react-three/drei'
 
 const AmbientParticles = () => {
   return (
     <div className="w-full h-full fixed inset-0 -z-10 pointer-events-none p-10">
       <Canvas shadows className="rounded-lg overflow-hidden">
-        <Sparkles count={350} scale={5} size={2.5} speed={0.25} color={''} />
-        <Environment preset={'apartment'} background={true} backgroundBlurriness={1} />
+        <Sparkles count={50} scale={5} size={0.5} speed={0.15} color={''} />
+        {/* <Environment preset={'warehouse'} background={true} backgroundBlurriness={1} /> */}
         {/* <directionalLight position={[0, 0, 5]} intensity={1} castShadow /> */}
         <PerspectiveCamera makeDefault position={[0, 1, 2.5]} />
       </Canvas>
